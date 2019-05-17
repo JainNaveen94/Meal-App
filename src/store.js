7536f2c372a87bf78446698feaad537276da6d1c
Naveen Jain <naveen.jain@nagarro.com>
2019-05-17 15:08:52 +0530

    API Hit Commit

--- src/store.js
+++ src/store.js
@@ -1,10 +1,36 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import axios from 'axios';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {}
+    state: {
+        recipes: [],
+        apiUrl: 'https://api.edamam.com/search'
+    },
+    mutations: {
+        setRecipes(state, payload) {
+            state.recipes = payload;
+        }
+    },
+    actions: {
+        async getRecipes({ state, commit }, plan) {
+            try {
+                let response = await axios.get(`${state.apiUrl}`, {
+                    params: {
+                        q: plan,
+                        app_id: 'd2558efa',
+                        app_key: 'ed89126bb9ee784e2f1c6b74df825b33',
+                        from: 0,
+                        to: 9
+                    }
+                });
+                commit('setRecipes', response.data.hits);
+            } catch (error) {
+                commit('setRecipes', []);
+            }
+        }
+    }
 });
