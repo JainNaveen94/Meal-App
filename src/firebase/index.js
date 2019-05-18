fc033e5eb355a90989b550ed6bb0ef7a6c5dfdc5
Naveen Jain <naveen.jain@nagarro.com>
2019-05-18 14:22:18 +0530

    Join Commit

---
+++ src/firebase/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBzwIUihDXizym8ADpGjwnCKKAPXbjJZa0",
+    authDomain: "meal-app-27a51.firebaseapp.com",
+    databaseURL: "https://meal-app-27a51.firebaseio.com",
+    projectId: "meal-app-27a51",
+    storageBucket: "meal-app-27a51.appspot.com",
+    messagingSenderId: "301572172965",
+    // appId: "1:301572172965:web:ba4f8dc08954c10f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
